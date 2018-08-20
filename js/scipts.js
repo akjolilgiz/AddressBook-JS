b5d8b3a3564e1b786e63a6554df26e14b5ee6921
Akjol Jaenbai and Connor McCarthy <akjol+connor@>
2018-08-20 08:41:50 -0700

    finish

---
+++ js/scipts.js
@@ -0,0 +1,39 @@
+//business logic
+function Contact (first, last, email){
+  this.firstName = first;
+  this.lastName = last;
+  this.email = email;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event){
+  event.preventDefault();
+
+  var inputtedFirstName = $("input#new-first-name").val();
+  var inputtedLastName = $("input#new-last-name").val();
+  var inputtedEmail = $("input#new-email").val();
+
+  var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail);
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+  $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.firstName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        $(".email").text(newContact.email);
+
+      });
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-email").val("");
+
+});
+});
